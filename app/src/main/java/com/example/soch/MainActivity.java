3b40a8b21b93d2b3ebebda3cad6e7bc22a067a3b
07rameen <rameenhamidd257@gmail.com>
2023-01-08 16:15:16 +0500

    updated sign up

--- app/src/main/java/com/example/soch/MainActivity.java
+++ app/src/main/java/com/example/soch/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.soch;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //ActivityMainBinding binding;
     private Button SubmitDetails,readCourseBtn;
     private DBHandler dbHandler;
+    int hour,minute;
     private EditText NameEdt, AgeEdt, MedEdt;
 
     @Override
@@ -27,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         NameEdt = findViewById(R.id.patientname);//edittext
         AgeEdt = findViewById(R.id.patientage);
         MedEdt = findViewById(R.id.medicationTime);
+
+        MedEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeset();
+            }
+
+        });
+
 
         dbHandler = new DBHandler(MainActivity.this);
       //  dbHandler.OnUpgrade();
@@ -55,6 +69,21 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
+
+    private void timeset() {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                hour = i;
+                minute=i1;
+                MedEdt.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
+
+            }
+        };
+        TimePickerDialog timePickerDialog=new TimePickerDialog(this,onTimeSetListener,hour,minute,true);
+        timePickerDialog.show();
+    }
+
     public void signup(View view) {
         Intent intent = new Intent(this,Dashboard.class);
         //intent.putExtra(data jaiga ismein to second page)
